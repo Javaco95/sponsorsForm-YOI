00687a32a4537bf75779cb0f106fff4859476f98
Javaco95 <127981406+Javaco95@users.noreply.github.com>
2023-07-17 13:27:21 -0600

    act

--- Sponsors/settings.py
+++ Sponsors/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
 
 STATICFILES_DIRS = [
